52b5665c9c9ba4b1bd3c1941456850bd8b4cb0ab
hyeon924 <hyeonn4713@gmail.com>
2024-11-03 18:36:17 +0900

    main add

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import { eventWrapper } from '@testing-library/user-event/dist/utils';
+import React from 'react';
+
+function Main() {
+    return(
+        <>
+            <h2>react</h2>
+        </>
+    );
+}
+
+export default Main;
